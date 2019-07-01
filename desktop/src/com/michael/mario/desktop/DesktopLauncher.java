3f0be46975a5e2293e8eee8459555cc5f8c71b47
michael perez <mp223415@gmail.com>
2019-07-01 13:57:33 -0700

    my laptop didnt crash

---
+++ desktop/src/com/michael/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.michael.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.michael.mario.Mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mario(), config);
+	}
+}
